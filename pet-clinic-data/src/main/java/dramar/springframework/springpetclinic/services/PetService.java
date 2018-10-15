f59b6529b7e7d3df53bca6557252fc9c682960c9
MrDragos <sdragos92@gmail.com>
2018-10-16 00:38:52 +0300

    Created service interfaces

---
+++ pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/PetService.java
@@ -0,0 +1,13 @@
+package dramar.springframework.springpetclinic.services;
+
+import dramar.springframework.springpetclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package dramar.springframework.springpetclinic.services;
+
+import dramar.springframework.springpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
