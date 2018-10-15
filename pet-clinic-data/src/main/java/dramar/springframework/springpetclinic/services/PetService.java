9af5143dd5f112b8c197a0c17228a813ba889018
MrDragos <sdragos92@gmail.com>
2018-10-16 00:53:56 +0300

    refactoring

--- pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/PetService.java
@@ -2,12 +2,6 @@ package dramar.springframework.springpetclinic.services;
 
 import dramar.springframework.springpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
--- pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/dramar/springframework/springpetclinic/services/VetService.java
@@ -2,13 +2,6 @@ package dramar.springframework.springpetclinic.services;
 
 import dramar.springframework.springpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long> {
 
-public interface VetService {
-
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
 }
